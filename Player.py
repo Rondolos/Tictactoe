ad853e268a4d1436c541603d0a5f2d543a114882
Rondolos <93493497+Rondolos@users.noreply.github.com>
2021-11-16 09:26:03 +0100

    Updated with smart computer player

--- Player.py
+++ Player.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Sun Oct 31 06:43:29 2021
@@ -12,6 +13,8 @@ Created on Sun Oct 31 06:43:29 2021
 
 import random
 import game_engine
+import time
+import copy
 
 class player():
     
@@ -28,6 +31,26 @@ class player():
         self.get_action = False
         
         
+class Human_player_tictactoe(player):
+    def __init__(self, player, order, color):
+        #This calls the methods from player
+        super().__init__(player, order, color)
+        #this assigns the letter based on order
+        if self.order == 0:
+            self.letter = 'X'
+        else:
+            self.letter = 'O'
+            
+    def get_move(self,game):
+        Legal_choice = False
+        while Legal_choice == False:
+            choice = int(input('Choose an option : ')) #This should be extended for checkers or chess
+            if any(x == choice for x in game.available_moves()):
+                Legal_choice = True
+            else:
+                print('the option was not available, choose from '+ str(game_engine.tictactoe.available_moves(game)))
+        return choice
+        
         
 class Computer_random_tictactoe(player):
     def __init__(self, player, order, color):
@@ -40,29 +63,124 @@ class Computer_random_tictactoe(player):
     def get_move(self,game):
         choice = random.choice(game.available_moves())
         return choice
-    
-    
-class Human_player_tictactoe(player):
+
+class Computer_smart_tictactoe(player):
+    #initializes a smart computer player
     def __init__(self, player, order, color):
-        #This calls the methods from player
         super().__init__(player, order, color)
-        #this assigns the letter based on order
         if self.order == 0:
             self.letter = 'X'
         else:
             self.letter = 'O'
+    
+    def get_move(self,game):
+        if len(game.available_moves()) == len(game.board):
+            choice = random.choice(game.available_moves())
+        else:
+            choice = self.minmax_init(game)
+        return choice
+        
+        
+    def minmax_init(self,game):
+        #print('minmax is initiated')
+        state = copy.deepcopy(game)
+        Remaining_moves = copy.deepcopy(state.available_moves()) 
+        #print('available moves at minmax iniation : ')
+        #print(Remaining_moves)
+        
+        #This creates a list where the scores of each move are placed
+        self.scores_you = [0 for _ in range(len(Remaining_moves))]
+        self.scores_opponent = [0 for _ in range(len(Remaining_moves))]
+        
+        #This for loop, loops through each possible move in Remaining moves, and memorizes the index of the move
+        test = 0
+        for loc, move in enumerate(Remaining_moves):
+            test += 1
+            #print('iteration ' +str(test))
+            letter = self.letter
+            #print(move)
+            state.change_game_board(move,letter)
+            #state.draw_board_command_line()
+            self.minmax(state,move,letter,loc) 
+            state.board[move] = ' '
+        
+        print(self.scores_you)
+        print(self.scores_opponent)
+        
+
+        winning_score = [x - self.scores_opponent[i] for i,x in enumerate(self.scores_you)]
+        print(winning_score)
+        
+        if max(winning_score) < 0:
+            print('imhere')
+            return Remaining_moves[self.scores_opponent.index(min(self.scores_opponent))] 
+        else:
+            return Remaining_moves[winning_score.index(max(winning_score))] 
+        
+        
+        
+    def minmax(self,state,move,letter,loc):
+        #change the board with the new input
+        #print(letter)  
+        #Check if the move is a winning move or a draw
+        
+        state.condition_checker_tictactoe(letter,move)
+        Remaining_moves = state.available_moves()
+        
+        
+        if state.Game_won == 1:
+            #print(letter +' won')
+            if self.letter == letter:
+                if self.scores_you[loc] < 1 + 1*state.board.count(' '):
+                    self.scores_you[loc] = 1 + 1*state.board.count(' ')
+                    #print(self.scores_you)
+                    
+            else:
+                if self.scores_opponent[loc] < (1 + 1*state.board.count(' ')):
+                    self.scores_opponent[loc] = (1 + 1*state.board.count(' '))
+                    #print(self.scores_opponent)
+            #print(self.scores)
+            state.board[move] = ' '
+        
+        if state.Game_draw == 1:
+            #print('its a draw')
+            state.board[move] = ' '
 
         
-    def get_move(self,game):
-        Legal_choice = False
-        while Legal_choice == False:
-            choice = int(input('Choose an option : ')) #This should be extended for checkers or chess
-            if any(x == choice for x in game.available_moves()):
-                Legal_choice = True
-            else:
-                print('the option was not available, choose from '+ str(game_engine.tictactoe.available_moves(game)))
-        return choice
+        if letter == 'X':
+            letter = 'O'
+        else:
+            letter = 'X'
+        
+        #print(self.scores)
+        if state.Game_won == 0 and state.Game_draw == 0:
+            for x in Remaining_moves:
+                #print(Remaining_moves)
+            
+                state.change_game_board(x,letter)
+                #state.draw_board_command_line()
+                #print('next recursion')
+                self.minmax(state,x,letter,loc)
+            
+                if state.Game_won == 0 and state.Game_draw == 0:
+                    #print('This gets passed')
+                    state.board[x] = ' '
+                    
+                #print('step back after recursion')
+                #state.draw_board_command_line()
+                
+            
+           
+            
+        state.Game_won = 0 #This resets the Game_won to 0
+        state.Game_draw = 0 #This resets the draw
+            
+            
+            #time.sleep(2)
+        
 
+            
+                
 #if __name__ == "__main__":
     #a = Human_player_tictactoe('Thijn', 0 ,'red')
     #available_player.param_objects.append(a)
@@ -70,5 +188,6 @@ class Human_player_tictactoe(player):
     #test = a.get_move(tictactoe)
     #b = Computer_random_tictactoe('Yaniv',1,'Blue')
     #available_player.param_objects.append(b)
+
 
 
--- game_engine.py
+++ game_engine.py
@@ -9,7 +9,7 @@ Created on Sun Oct 31 07:06:50 2021
 # Every game has a number of actions that are not unique to the gameboard
 import Player
 import sys
-import math
+import time
 
 class game:
     def __init__(self):
@@ -23,14 +23,13 @@ class game:
         self.lenx = lenx
         self.leny = leny
         
-    def change_game_board(self,change):
+    def change_game_board(self,location,letter):
         #This expects to receive a list of tuples, (zero,zero), which will be read as follows
         # if (1,#), then that location in the list will change, # denotes what will be placed in the list
         # for tictactoe the tuple is not needed, but if I want to create checkers I need something like this
-        for x in range(len(self.board)):
-            var = change[x]
-            if var[0] == 1:
-                self.board[x] = var[1]
+        #print(letter)
+        #print(location)
+        self.board[location] = letter
     
     def draw_board_command_line(self):
     #Draws the command line board based on inputs from game
@@ -76,41 +75,40 @@ class tictactoe(game):
     def available_moves(self):
          return [i for i, spot in enumerate(self.board) if spot == ' '] #this can be used to check if the move is legal, or to only allow these moves to be chosen
 
-    def condition_checker_tictactoe(self,player,letter,location):
+    def condition_checker_tictactoe(self,letter,location):
         #this checks for a winning move
         #first check rows
+        #print('Rows')
         row_index = location//self.lenx
         row = self.board[row_index*self.lenx:(row_index+1)*self.lenx]
-
         if all([x == letter for x in row]):
             self.Game_won = 1
             
         #now check columns
+        #print('Columns')
         column_index = location % self.lenx #deze moet ik even uitzoeken
         column = [self.board[column_index+i*self.lenx] for i in range(self.lenx)]
         #print(column)
         if all([x == letter for x in column]):
-            print('column won')
             self.Game_won = 1
         
         #now check diagonals
-        if location % 2 == 0:
-            diagonal1 = [self.board[i] for i in [0,4,8]]
-            if all(x == letter for x in diagonal1):
-                #print('diagonal1 won')
-                self.Game_won = 1
-            diagonal2 = [self.board[i] for i in [2,4,6]]
-            if all(x == letter for x in diagonal2):  
-                #print('diagonal2 won')
-                self.Game_won = 1
+        #print('Diagonals')
+        
+        diagonal1 = [self.board[i] for i in [0,4,8]]
+        if all(x == letter for x in diagonal1):
+            #print('diagonal1 won')
+            self.Game_won = 1
+        diagonal2 = [self.board[i] for i in [2,4,6]]
+        if all(x == letter for x in diagonal2):  
+            #print('diagonal2 won')
+            self.Game_won = 1
             
         #this checks for a draw
         if any(x == ' ' for x in self.board):
             self.Game_draw = 0
         else: 
             self.Game_draw = 1
-            
-        self.Check_conditions(player)
         
         
 def tictactoe_play(game):
@@ -119,10 +117,11 @@ def tictactoe_play(game):
         
         if input('Do you want to add a computer player : ') == 'Yes':
             #a = Player.Computer_random_tictactoe(input('Name computer player : '),input(' 0 = X, 1 = O : '),input('color : '))
-            a = Player.Computer_random_tictactoe('Thijn',0,'red')
+            a = Player.Human_player_tictactoe('Thijn',0,'red')
         else:
             #a = Player.Human_player_tictactoe(input('Name human player : '),input(' 0 = X, 1 = O : '),input('color : '))
-            a = Player.Human_player_tictactoe('Yaniv',1,'blue')
+            a = Player.Computer_smart_tictactoe('Yaniv',1,'blue')
+            
         if a.order == 0:
             a.letter = 'X'
         else:
@@ -131,24 +130,55 @@ def tictactoe_play(game):
         game.players.append(a)
     
     while game.Game_won == 0 and game.Game_draw == 0:
-        change = [(0,0) for x in range(len(game.board))]
         for x in game.players:
             print('Its ' + x.player + '\'s turn')
             
             location = x.get_move(game)
-            change[location] = (1,x.letter)
-            game.change_game_board(change)
-            
+            print(location)
+            game.change_game_board(location,x.letter)
+            print(game.available_moves())
             game.draw_board_command_line()
             
             #This checks the conditions at the end of the players turn
-            game.condition_checker_tictactoe(x.player,x.letter,location)
-                
+            game.condition_checker_tictactoe(x.letter,location)
+            game.Check_conditions(x.player)
+            time.sleep(2)
+            
         game.Game_won = 0
         game.Game_draw = 0    
             
 
+def tictactoe_debug(game):
+    #This checks if there are two players, if not, it asks to add the players to the game
+
+    a = Player.Computer_smart_tictactoe('Thijn',0,'red')
+    b = Player.Computer_smart_tictactoe('Yaniv',1,'blue')
+
+
+    game.players.append(b)
+    game.players.append(a)
+    
+    game.board = ['O','X',' ',' ','X',' ',' ',' ',' ']
+    
+    game.draw_board_command_line()
+    
+    while game.Game_won == 0:
+        for x in game.players:
+            print('Its ' + x.player + '\'s turn')
             
+            location = x.get_move(game)
+            print(location)
+            game.change_game_board(location,x.letter)
+            print(game.available_moves())
+            game.draw_board_command_line()
+            
+            #This checks the conditions at the end of the players turn
+            game.condition_checker_tictactoe(x.letter,location)
+            game.Check_conditions(x.player)
+            time.sleep(2)
+        
+        game.Game_won = 0
+        game.Game_draw = 0                
     
 if __name__ == "__main__":
     a = tictactoe()
